a42be582dda25c81962db72b79d1fedf0004d846
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2025-03-02 03:57:48 -0500

    feat: increase performance of cpu when playing a game
    This follows the settings on the PSP (but may not be correct for other devices.

--- launch.sh
+++ launch.sh
@@ -221,8 +221,8 @@ configure_controls() {
 }
 
 configure_cpu() {
-	echo ondemand >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-	echo 1200000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+	echo performance >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+	echo 1608000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 	echo 1800000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 }
 
