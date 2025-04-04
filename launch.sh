82830899c90360e94dde6b12cd9ef3388dd021ce
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2025-04-04 00:31:50 -0400

    fix: restore cpu scaling on exit
    Closes #33

--- launch.sh
+++ launch.sh
@@ -224,11 +224,15 @@ get_rom_path() {
 			ROM_PATH="$*"
 			;;
 		*.zip | *.7z)
+			existing_governor="$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)"
+			existing_max_freq="$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq)"
 			echo performance >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 			echo 1800000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 			ROM_PATH="$TEMP_ROM"
 
 			7zzs e "$*" -so >"$TEMP_ROM"
+			echo "$existing_governor" >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+			echo "$existing_max_freq" >/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 			;;
 	esac
 
@@ -295,6 +299,10 @@ configure_controls() {
 
 configure_cpu() {
 	cpu_mode="$(get_cpu_mode)"
+
+	cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor >"$HOME/cpu_governor.txt"
+	cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq >"$HOME/cpu_min_freq.txt"
+	cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq >"$HOME/cpu_max_freq.txt"
 
 	if [ "$cpu_mode" = "performance" ]; then
 		echo performance >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
@@ -380,6 +388,19 @@ cleanup() {
 
 	rm -f /tmp/stay_awake
 	killall minui-presenter >/dev/null 2>&1 || true
+
+	if [ -f "$HOME/cpu_governor.txt" ]; then
+		cat "$HOME/cpu_governor.txt" >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+		rm -f "$HOME/cpu_governor.txt"
+	fi
+	if [ -f "$HOME/cpu_min_freq.txt" ]; then
+		cat "$HOME/cpu_min_freq.txt" >/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+		rm -f "$HOME/cpu_min_freq.txt"
+	fi
+	if [ -f "$HOME/cpu_max_freq.txt" ]; then
+		cat "$HOME/cpu_max_freq.txt" >/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+		rm -f "$HOME/cpu_max_freq.txt"
+	fi
 
 	# cleanup remap
 	rm -f /tmp/trimui_inputd/input_no_dpad
