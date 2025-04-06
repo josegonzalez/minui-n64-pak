03018885bfc19476e9635f956c1c60ce9ed9e0ba
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2025-04-06 01:13:18 -0400

    refactor: properly handle hold states
    Also background a timer so we handle powering off when evtest doesn't have any output for a while

--- launch.sh
+++ launch.sh
@@ -387,6 +387,7 @@ cleanup() {
 	rm -f "/tmp/minui-list"
 	rm -f "/tmp/mupen64plus.pid"
 	rm -f "/tmp/stay_awake"
+	rm -f "/tmp/force-power-off"
 	killall emit-key >/dev/null 2>&1 || true
 	killall minui-presenter >/dev/null 2>&1 || true
 
